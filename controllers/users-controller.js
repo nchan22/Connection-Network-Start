a2658398b389c7b26b6d0667d0dfb8684241f831
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-08 19:43:50 -0400

    set up users controller

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,2 +1,12 @@
 // Require Users Model
 const { Users } = require('../models');
+
+// Set up Users Controller
+const usersController = {
+
+    // Create a new User
+    createUsers({body}, res) {
+        Users.create(body)
+            .then(dbUsersData => res.json(dbUsersData))
+            .catch(err => res.status(400).json(err));
+    },
