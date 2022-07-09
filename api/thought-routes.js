5012c4695f6110531a1ee62f41160c3ebf754a95
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-08 20:07:50 -0400

    Added required npms and parts from other files

---
+++ api/thought-routes.js
@@ -0,0 +1,14 @@
+// Require express router
+const router = require('express').Router();
+
+// Set requirements (from thoughts-controller)
+const {
+    getAllThoughts,
+    getThoughtsById,
+    createThoughts,
+    updateThoughts,
+    deleteThoughts,
+    addReaction,
+    deleteReaction
+
+} = require('../controllers/thoughts-controller');
