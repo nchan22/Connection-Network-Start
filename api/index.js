25d334fc80696a87e175052222036d64dfd8034f
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-08 20:15:01 -0400

    Added user routes

--- api/index.js
+++ api/index.js
@@ -12,3 +12,15 @@ const {
     deleteFriend
 } = require('../controllers/users-controller');
 
+// -- Directs to: /api/users <GET, POST>
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUsers);
+
+// -- Directs to: /api/users/:id <GET, PUT, DELETE>
+router
+    .route('/:id')
+    .get(getUsersById)
+    .put(updateUsers)
+    .delete(deleteUsers);
